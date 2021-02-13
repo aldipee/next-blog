17bb03aabf805a298d8e64e391b5d60c7852f222
Aldi Pranata <aldipeee@gmail.com>
2021-02-13 23:13:50 +0700

    Add an Intersection Observer

--- components/TableOfContents/Toc.js
+++ components/TableOfContents/Toc.js
@@ -1,18 +1,18 @@
-import React from "react";
+import React from 'react';
 
 const TableOfContents = ({ body }) => {
   return (
-    <div className="sticky self-start top-8">
-      <h4 className="pb-2 mb-5 text-xl border-b border-gray-300 font-titleHomeMedium">Contents</h4>
-      <ul>
+    <aside className='sticky self-start top-8 '>
+      <h4 className='pb-2 mb-5 text-xl font-titleHomeMedium'>Contents</h4>
+      <ul className='border-l px-4 border-gray-300'>
         {body &&
           body.map((headings) => (
-            <li className={`${headings.depth > 2 ? "pl-4" : ""} mb-3 text-sm text-gray-500  hover:text-primaryBlue`}>
+            <li className={`${headings.depth > 2 ? 'pl-4' : ''} mb-3 text-xs text-gray-500  hover:text-primaryBlue`}>
               <a href={`#${headings.id}`}>{headings.title}</a>
             </li>
           ))}
       </ul>
-    </div>
+    </aside>
   );
 };
 
--- libs/content.js
+++ libs/content.js
@@ -1,12 +1,50 @@
 export function generateLinkMarkup($contentElement) {
-  const headings = [...$contentElement.querySelectorAll("h2, h3")];
+  console.log($contentElement, '$Content');
+  const headings = [...$contentElement.querySelectorAll('h2, h3')];
   const parsedHeadings = headings.map((heading) => {
     return {
       title: heading.innerText,
-      depth: heading.nodeName.replace(/\D/g, ""),
-      id: heading.getAttribute("id"),
+      depth: heading.nodeName.replace(/\D/g, ''),
+      id: heading.getAttribute('id'),
     };
   });
 
   return parsedHeadings;
+}
+
+function updateLinks(visibleId, $links) {
+  $links.map((link) => {
+    let href = link.getAttribute('href');
+    link.classList.remove('text-primaryBlue', 'font-semibold');
+    if (href === visibleId) link.classList.add('text-primaryBlue', 'font-semibold');
+  });
+}
+
+function handleObserver(entries, observer, $links) {
+  entries.forEach((entry) => {
+    const { target, isIntersecting, intersectionRatio } = entry;
+    if (isIntersecting && intersectionRatio >= 1) {
+      const visibleId = `#${target.getAttribute('id')}`;
+      updateLinks(visibleId, $links);
+    }
+  });
+}
+
+export function createObserver($links) {
+  const options = {
+    rootMargin: '0px 0px -200px 0px',
+    threshold: 1,
+  };
+  const callback = (e, o) => handleObserver(e, o, $links);
+  return new IntersectionObserver(callback, options);
+}
+
+export function initializeTOC($main, $aside) {
+  // Part 3
+  const $headings = [...$main.querySelectorAll('h2, h3')];
+  const motionQuery = window.matchMedia('(prefers-reduced-motion)');
+  console.log($aside, '$aside');
+  const $links = [...$aside.querySelectorAll('a')];
+  const observer = createObserver($links);
+  $headings.map((heading) => observer.observe(heading));
 }
