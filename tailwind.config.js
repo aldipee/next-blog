97e6514a060289425162128e45125cd913d9b8ec
Aldi Pranata <aldipeee@gmail.com>
2021-05-19 09:46:05 +0700

    Perf : Removing unused CSS Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 const colors = require('tailwindcss/colors');
 
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: {
+    enabled: true,
+    content: ['./pages/**/*.js','./pages/*.js', './components/**/*.js','./components/*.js'],
+  },
   presets: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
